445cdcb876b401ee74f1a151c78a0c2b92efe6d5
Shubhrant Dey <118185411+ShubhrantHotmail@users.noreply.github.com>
2023-11-01 12:00:24 +0530

    update

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Signup from "./ui/Signup";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
           <Route path="/" element={<Signup />} />
         </Routes>
